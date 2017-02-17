5fd394f8403da3c8db61fa9b3819d9eaf549c277
Francisco Calaça Xavier <chicocx@gmail.com>
2017-02-17 17:33:50 -0200

    commit inicial

---
+++ src/main/java/io/convergencia/aulamaven/App.java
@@ -0,0 +1,13 @@
+package io.convergencia.aulamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/io/convergencia/aulamaven/ExemploSelect.java
@@ -0,0 +1,30 @@
+package io.convergencia.aulamaven;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ExemploSelect {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Class.forName("org.postgresql.Driver");
+		
+		try (Connection con = DriverManager.getConnection("jdbc:postgresql:exemplojdbc", "postgres", "123456")) {
+			
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from contato");
+			while(rs.next()){
+				System.out.printf("%5d %-20s %-20s\n", rs.getInt("id"), rs.getString("nome"), rs.getString("telefone"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+}
